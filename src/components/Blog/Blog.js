abe1c0ff53336ea49d212da6d0386491e696804c
Pervez <pervezhossain1998@gmail.com>
2022-11-08 17:24:09 +0600

    3rd question addedd

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -8,7 +8,9 @@ const Blog = () => {
             <h2 className='text-3xl font-bold'>Difference between SQL and NoSQL</h2>
             <p className='text-xl font-bold mt-4 mb-8'>SQL is the programming language used to interface with relational databases. <br/>NoSQL is a class of DBMs that are non-relational and generally do not use SQL.</p>
             <h2 className='text-3xl font-bold'>What is JWT, and how does it work</h2>
-            <p className='text-xl font-bold mt-4  mb-8'>SON Web Token (JWT) is an open standard (RFC 7519) for securely transmitting information between parties as JSON object. It is compact, readable and digitally signed using a private key/ or a public key pair by the Identity Provider(IdP).</p>
+            <p className='text-xl font-bold mt-4  mb-8'>JSON Web Token (JWT) is an open standard (RFC 7519) for securely transmitting information between parties as JSON object.<br/> It is compact, readable and digitally signed using a private key/ or a public key pair by the Identity Provider(IdP).</p>
+            <h2 className='text-3xl font-bold'>What is the difference between javascript and NodeJS?</h2>
+            <p className='text-xl font-bold mt-4  mb-8'>JavaScript is a simple programming language that can be used with any browser that has the JavaScript Engine installed.<br/> Node. js, on the other hand, is an interpreter or execution environment for the JavaScript programming language.</p>
         </div>
     );
 };
