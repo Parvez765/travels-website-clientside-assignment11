b0ee19be7e5ed16c9a06e6f3611dd17195fc1777
Pervez <pervezhossain1998@gmail.com>
2022-11-08 17:25:21 +0600

    4th Question added

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -11,6 +11,8 @@ const Blog = () => {
             <p className='text-xl font-bold mt-4  mb-8'>JSON Web Token (JWT) is an open standard (RFC 7519) for securely transmitting information between parties as JSON object.<br/> It is compact, readable and digitally signed using a private key/ or a public key pair by the Identity Provider(IdP).</p>
             <h2 className='text-3xl font-bold'>What is the difference between javascript and NodeJS?</h2>
             <p className='text-xl font-bold mt-4  mb-8'>JavaScript is a simple programming language that can be used with any browser that has the JavaScript Engine installed.<br/> Node. js, on the other hand, is an interpreter or execution environment for the JavaScript programming language.</p>
+            <h2 className='text-3xl font-bold'>How does NodeJS handle multiple requests at the same time?</h2>
+            <p className='text-xl font-bold mt-4  mb-8'>NodeJS receives multiple client requests and places them into EventQueue. NodeJS is built with the concept of event-driven architecture.<br/> NodeJS has its own EventLoop which is an infinite loop that receives requests and processes them.</p>
         </div>
     );
 };
